0f51edd58f01393d06a8cf25d3195b9a78a3a322
Ch3LL <megan.wilhite@gmail.com>
2019-10-07 17:30:03 -0400

    Add tests for auto download artifacts

---
+++ tests/unit/heist/conftest.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+    tests.unit.heist.conftest
+    ~~~~~~~~~~~~~~
+
+    Setup fixtures specifically for the heist unit tests
+'''
+
+# Import python libs
+import sys
+
+# Import local libs
+import tests.helpers.mock_hub as helpers
+
+# Import 3rd-party libs
+import mock
+import pytest
+import pop
+import pop.utils.testing as testing
+
+@pytest.fixture(scope='function')
+def hub():
+    '''
+    Add required subs to the hub.
+    '''
+    hub = pop.hub.Hub()
+    hub.OPT = {'heist': {}}
+    with mock.patch.object(sys, 'argv', sys.argv[:1]):
+        hub.pop.sub.add('heist.heist')
+
+    hub.pop.sub.add(dyne_name='tunnel')
+    yield hub
+    hub.heist.init.clean()
+
+@pytest.fixture
+def mock_hub() -> testing.MockHub:
+    '''
+    mock the needed subs for the heist tests
+    '''
+    # A fixture is required for asynchronous tests to access a mock_hub
+    return helpers.mock_hub(subs=['heist.heist', 'roster', 'tunnel'])
--- tests/unit/heist/test_salt_master.py
+++ tests/unit/heist/test_salt_master.py
@@ -6,19 +6,15 @@ from typing import Any, Dict, Tuple
 
 # Import Local libs
 import heist.heist.salt_master
-import tests.helpers.mock_hub as helpers
+import heist.tunnel.asyncssh_tunnel
 
 # Import 3rd-party libs
+import asyncssh.process
 import mock
 import pop.utils.testing as testing
 import pop.hub
 import pytest
-
-
-@pytest.fixture
-def mock_hub() -> testing.MockHub:
-    # A fixture is required for asynchronous tests to access a mock_hub
-    return helpers.mock_hub(subs=['heist.heist', 'roster', 'tunnel'])
+import tarfile
 
 
 @pytest.fixture
@@ -67,8 +63,8 @@ class TestSaltMaster:
         # Setup
         artifacts_dir = 'art'
         mock_hub.OPT = {'heist': {'artifacts_dir': artifacts_dir,
-                                 'checkin_time': 1,
-                                 'dynamic_upgrade': False}}
+                                  'checkin_time': 1,
+                                  'dynamic_upgrade': False}}
         minion = os.path.join(artifacts_dir, 'salt-minion.pex')
         pytar = os.path.join(artifacts_dir, 'py374.txz')
         minion_config, _ = mk_config_data
@@ -84,22 +80,112 @@ class TestSaltMaster:
         # Verify
         mock_hub.tunnel.asyncssh.create.assert_called_once_with(t_name, remote)
         for cmd in [
-            f'mkdir -p {os.path.join(run_dir, "conf")}',
-            f'mkdir -p {os.path.join(run_dir, "root")}',
-            f'tar -xvf {os.path.join(run_dir, "py3.txz")} -C {run_dir}',
-            f'{os.path.join(run_dir, "py3", "bin", "python3")} {os.path.join(run_dir, "salt-minion.pex")} --config-dir {os.path.join(run_dir, "conf")}',
+                f'mkdir -p {os.path.join(run_dir, "conf")}',
+                f'mkdir -p {os.path.join(run_dir, "root")}',
+                f'tar -xvf {os.path.join(run_dir, "py3.txz")} -C {run_dir}',
+                f'{os.path.join(run_dir, "py3", "bin", "python3")} {os.path.join(run_dir, "salt-minion.pex")} --config-dir {os.path.join(run_dir, "conf")}',
         ]:
             mock_hub.tunnel.asyncssh.cmd.assert_any_call(t_name, cmd)
 
         for send in [
-            (minion_config, os.path.join(run_dir, 'conf', 'minion')),
-            (minion, os.path.join(run_dir, 'salt-minion.pex')),
-            (pytar, os.path.join(run_dir, 'py3.txz')),
+                (minion_config, os.path.join(run_dir, 'conf', 'minion')),
+                (minion, os.path.join(run_dir, 'salt-minion.pex')),
+                (pytar, os.path.join(run_dir, 'py3.txz')),
         ]:
             mock_hub.tunnel.asyncssh.send.assert_any_call(t_name, *send)
 
         for tunnel in [
-            (44505, 4505),
-            (44506, 4506),
+                (44505, 4505),
+                (44506, 4506),
         ]:
             mock_hub.tunnel.asyncssh.tunnel.assert_any_call(t_name, *tunnel)
+
+    @pytest.mark.asyncio
+    async def test_detect_os(self,
+                             mock_hub):
+        '''
+        test heist.heist.salt_master.detect_os
+        when returncode is 0 and os is linux
+        '''
+        cmd_ret = asyncssh.process.SSHCompletedProcess(
+            command={'uname -a'},
+            exit_status=0,
+            returncode=0,
+            stdout='Linux test-name 5.0',
+            stderr='')
+
+        # Setup
+        t_name = secrets.token_hex()
+        mock_hub.tunnel.asyncssh.cmd.return_value = cmd_ret
+        ret = await heist.heist.salt_master.detect_os(mock_hub, t_name,
+                                                      'asyncssh')
+        assert ret == 'linux'
+
+    @pytest.mark.asyncio
+    async def test_detect_os_bad_return(self,
+                                        mock_hub: testing.MockHub):
+        '''
+        test heist.heist.salt_master.detect_os
+        when returncode is not 0 and os is linux
+        '''
+        cmd_ret = asyncssh.process.SSHCompletedProcess(
+            command={'uname -a'},
+            exit_status=0,
+            returncode=127,
+            stdout='',
+            stderr='zsh:1: command not found: name\n')
+
+        # Setup
+        t_name = secrets.token_hex()
+        mock_hub.tunnel.asyncssh.cmd.return_value = cmd_ret
+        ret = await heist.heist.salt_master.detect_os(mock_hub, t_name,
+                                                      'asyncssh')
+        assert not ret
+
+    @pytest.mark.asyncio
+    async def test_get_version_pypi(self,
+                                    hub):
+        '''
+        test heist.heist.salt_master.get_version_pypi
+        with artifact_version set
+        '''
+        saltbin = '2019.2.1'
+        hub.OPT = {'heist': {'artifact_version': '2019.2.1'}}
+        ver, data = await heist.heist.salt_master.get_version_pypi(hub, 'linux')
+        assert ver == saltbin
+        assert saltbin in data['releases']
+
+    @pytest.mark.asyncio
+    async def test_get_artifact(self,
+                                mock_hub: testing.MockHub,
+                                tmp_path):
+        '''
+        test heist.salt_master.get_artifact
+        when artifact does not already exist
+        '''
+        t_name = secrets.token_hex()
+        ver = '2019.2.1'
+        tar_f = f'salt-{ver}.tar.gz'
+        data = {'releases': {ver: {0: {'url': f'https://pypi.org/project/saltbin/{tar_f}'}}}}
+
+        # create test tar file
+        files = ['PKG-INFO', f'salt-{ver}']
+        tar_l = os.path.join(tmp_path, f'salt-{ver}.tar.gz')
+
+        with tarfile.open(tar_l, "w:gz") as tar:
+            for fn_ in files:
+                tmp = tmp_path / fn_
+                tmp.write_text(f'test-info for {fn_}')
+                tar.add(tmp, arcname=os.path.basename(tmp))
+
+        [os.remove(os.path.join(tmp_path, x)) for x in files]
+
+        mock_hub.heist.salt_master.fetch.return_value = tar_l
+        mock_hub.heist.salt_master.latest.return_value = False
+        ret = await heist.heist.salt_master.get_artifact(mock_hub,
+                                                         t_name,
+                                                         'asyncssh',
+                                                         tmp_path,
+                                                         ver,
+                                                         data)
+        assert ret
