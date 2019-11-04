d312d3864e6ef2cb50be312b03519fd2707a6a3b
Thomas S Hatch <thatch45@gmail.com>
2019-11-04 11:28:17 -0700

    bump to version 2.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Thomas S Hatch'
 author = 'Thomas S Hatch'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
